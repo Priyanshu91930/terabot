460a3d53bc9bccbdde3762640d79be046cd9e89d
priyanshu solanki <131973299+Priyanshu91930@users.noreply.github.com>
2026-08-29 12:21:00 +0530

    feat: add 2 channel buttons + group button to /start

--- main.py
+++ main.py
@@ -162,10 +162,11 @@ Let's make your video experience even better!
         parse_mode="markdown",
         buttons=[
             [
-                Button.url("Channel 📢", url="https://t.me/TeraboxDownloaderINDIA"),
-                Button.url("Group 💬", url="https://t.me/+L7tcuoCsTaMxZWVl"),
+                Button.url("Channel 1 📢", url="https://t.me/TeraboxDownloaderINDIA"),
+                Button.url("Channel 2 📢", url="https://t.me/+L7tcuoCsTaMxZWVl"),
             ],
             [
+                Button.url("Group 💬", url="https://t.me/+cySPj7iDogFkMzc1"),
                 Button.url("Bot 🤖", url=f"https://t.me/{BOT_USERNAME}"),
             ],
         ],
